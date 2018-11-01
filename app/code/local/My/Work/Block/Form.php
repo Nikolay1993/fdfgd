4dc1db92364d7291c85b2a5f4ada1b2fe79bc8f8
Николай <yboomzi@mail.ru>
2018-11-01 16:34:12 +0200

    4 task completed

---
+++ app/code/local/My/Work/Block/Form.php
@@ -0,0 +1,8 @@
+<?php
+
+class My_Work_Block_Form extends Mage_Core_Block_Template
+{
+
+
+
+}
--- app/code/local/My/Work/controllers/IndexController.php
+++ app/code/local/My/Work/controllers/IndexController.php
@@ -9,4 +9,27 @@ class My_Work_IndexController extends Mage_Core_Controller_Front_Action
         $this->renderLayout();
     }
 
+    public function saveAction()
+    {
+        $name = $this->getRequest()->getPost('name');
+        $question = $this->getRequest()->getPost('question');
+
+        if(isset($name) && ($name!='')  && isset($question) && ($question!='') ) {
+
+            $model = Mage::getModel('mywork/work');
+            $model->setName($name);
+            $model->setQuestion($question);
+            $model->setSatus(My_Work_Model_Source_Status::PROCESSING);
+            if($model->save() == true){
+                Mage::getSingleton('core/session')->addSuccess('massage successful');
+            }
+            if($model->save() != true){
+                Mage::getSingleton('core/session')->addError('error input');
+            }
+
+        }
+
+        $this->_redirect('faq/index/index');
+    }
+
 }
